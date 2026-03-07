21f39da22b1a46376d1d2b8650bfa3523f8dfed4
tigran.movsisyan <sgta1980@gmail.com>
2026-03-07 21:21:47 +0300

    Added currency symbol

--- domain/src/main/java/io/github/t1geryan/domain/models/Currency.kt
+++ domain/src/main/java/io/github/t1geryan/domain/models/Currency.kt
@@ -1,6 +1,10 @@
 package io.github.t1geryan.domain.models
 
-enum class Currency(val code: String, val subUnitMultiplier: Float = 0.01f) {
-    RUSSIAN_RUBLE("RUB"),
+enum class Currency(
+    val code: String,
+    val symbol: String,
+    val subUnitMultiplier: Float = 0.01f,
+) {
+    RUSSIAN_RUBLE("RUB", "\u20bd"),
     ;
 }
