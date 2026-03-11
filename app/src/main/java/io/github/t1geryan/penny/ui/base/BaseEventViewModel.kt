468e134d14fc8ecbb628b69b5d56de3662065e21
tigran.movsisyan <sgta1980@gmail.com>
2026-03-11 14:42:27 +0300

    Updated BaseEventViewModel.kt

--- app/src/main/java/io/github/t1geryan/penny/ui/base/BaseEventViewModel.kt
+++ app/src/main/java/io/github/t1geryan/penny/ui/base/BaseEventViewModel.kt
@@ -1,12 +1,15 @@
 package io.github.t1geryan.penny.ui.base
 
+import androidx.lifecycle.viewModelScope
 import io.github.t1geryan.mvi.Event
 import io.github.t1geryan.mvi.EventSender
 import io.github.t1geryan.mvi.Intent
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 abstract class BaseEventViewModel<I : Intent, S : Any, E : Event>(initialState: S) :
     BaseViewModel<I, S>(initialState),
@@ -14,4 +17,10 @@ abstract class BaseEventViewModel<I : Intent, S : Any, E : Event>(initialState: 
 
     protected val _events = Channel<E>(Channel.UNLIMITED, BufferOverflow.DROP_LATEST)
     override val events: Flow<E> = _events.receiveAsFlow()
+
+    protected fun sendEvent(event: E) {
+        viewModelScope.launch(Dispatchers.Main.immediate) {
+            _events.send(event)
+        }
+    }
 }
