29cfe089ed2a0634c195312f4ae245b7fc35d3ff
tigran.movsisyan <sgta1980@gmail.com>
2026-05-06 11:50:58 +0300

    [PNYM-15] Implemented permission request

--- app/src/main/java/io/github/t1geryan/penny/ui/features/root/RootComponent.kt
+++ app/src/main/java/io/github/t1geryan/penny/ui/features/root/RootComponent.kt
@@ -3,8 +3,13 @@ package io.github.t1geryan.penny.ui.features.root
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import io.github.t1geryan.penny.ui.navigation.graph.RootNavGraph
+import io.github.t1geryan.penny.ui.permissions.RequestPushNotificationsPermission
 
 @Composable
 fun RootComponent(modifier: Modifier = Modifier) {
     RootNavGraph(modifier = modifier)
+
+    RequestPushNotificationsPermission {
+        // no-op
+    }
 }
---
+++ app/src/main/java/io/github/t1geryan/penny/ui/permissions/PushNotifications.kt
@@ -0,0 +1,33 @@
+package io.github.t1geryan.penny.ui.permissions
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+
+@Composable
+fun RequestPushNotificationsPermission(onResult: (isGranted: Boolean) -> Unit) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val context = LocalContext.current
+        val permissionLauncher = rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+            onResult,
+        )
+
+        LaunchedEffect(Unit) {
+            if (
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+}
